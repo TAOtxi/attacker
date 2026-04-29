8626ad80e384760d4b086e55ff111c27b5cc16e0
甚吊圣人 <1321075268@qq.com>
2026-04-29 09:16:13 +0800

    mod: 更新作者信息

---
+++ src/main/java/cn/taotxi/Implement/ModMenuIntegration.java
@@ -0,0 +1,12 @@
+package cn.taotxi.Implement;
+
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+
+// TODO: 待实现配置界面
+// public class ModMenuIntegration implements ModMenuApi {
+//     @Override
+//     public ConfigScreenFactory<?> getModConfigScreenFactory() {
+//         return ConfigScreen::getConfigScreen;
+//     }
+// }
